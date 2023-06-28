ec2d3b0792bd2c711935fd1f7fe278aa1bfb4903
Martin J. Pollard <martin0566@gmail.com>
2023-06-27 21:54:33 -0400

    Lockscreen: Remove keyboard shortcut settings
    A version of this is now in ThemePicker as of QPR3.

--- src/com/altair/settings/fragments/CustomLockscreenSettings.java
+++ src/com/altair/settings/fragments/CustomLockscreenSettings.java
@@ -42,17 +42,6 @@ public class CustomLockscreenSettings extends DashboardFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "CustomUserInterfaceSettings";
 
-    private static final String KEY_SHORTCUT_START_KEY = "lockscreen_shortcut_start";
-    private static final String KEY_SHORTCUT_END_KEY = "lockscreen_shortcut_end";
-    private static final String KEY_SHORTCUT_ENFORCE_KEY = "lockscreen_shortcut_enforce";
-
-    private static final String[] DEFAULT_START_SHORTCUT = new String[] { "home", "flashlight", "do_not_disturb" };
-    private static final String[] DEFAULT_END_SHORTCUT = new String[] { "wallet", "qr_code_scanner", "camera" };
-
-    private ListPreference mStartShortcut;
-    private ListPreference mEndShortcut;
-    private SwitchPreference mEnforceShortcut;
-
     @Override
     protected int getPreferenceScreenResId() {
         return R.xml.menu_lockscreen_settings;
@@ -61,14 +50,6 @@ public class CustomLockscreenSettings extends DashboardFragment implements
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        mStartShortcut = findPreference(KEY_SHORTCUT_START_KEY);
-        mEndShortcut = findPreference(KEY_SHORTCUT_END_KEY);
-        mEnforceShortcut = findPreference(KEY_SHORTCUT_ENFORCE_KEY);
-        updateShortcutSelection();
-        mStartShortcut.setOnPreferenceChangeListener(this);
-        mEndShortcut.setOnPreferenceChangeListener(this);
-        mEnforceShortcut.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -98,84 +79,12 @@ public class CustomLockscreenSettings extends DashboardFragment implements
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mStartShortcut) {
-            setShortcutSelection((String) newValue, true);
-            return true;
-        } else if (preference == mEndShortcut) {
-            setShortcutSelection((String) newValue, false);
-            return true;
-        } else if (preference == mEnforceShortcut) {
-            final boolean value = (Boolean) newValue;
-            setShortcutSelection(mStartShortcut.getValue(), true, value);
-            setShortcutSelection(mEndShortcut.getValue(), false, value);
-            return true;
-        }
         return false;
     }
 
     @Override
     public boolean onPreferenceTreeClick(Preference preference) {
         return super.onPreferenceTreeClick(preference);
-    }
-
-    private String getSettingsShortcutValue() {
-        String value = Settings.System.getString(getActivity().getContentResolver(),
-                Settings.System.KEYGUARD_QUICK_TOGGLES_NEW);
-        if (value == null || value.isEmpty()) {
-            StringBuilder sb = new StringBuilder(DEFAULT_START_SHORTCUT[0]);
-            for (int i = 1; i < DEFAULT_START_SHORTCUT.length; i++) {
-                sb.append(",").append(DEFAULT_START_SHORTCUT[i]);
-            }
-            sb.append(";" + DEFAULT_END_SHORTCUT[0]);
-            for (int i = 1; i < DEFAULT_END_SHORTCUT.length; i++) {
-                sb.append(",").append(DEFAULT_END_SHORTCUT[i]);
-            }
-            value = sb.toString();
-        }
-        return value;
-    }
-
-    private void updateShortcutSelection() {
-        final String value = getSettingsShortcutValue();
-        final String[] split = value.split(";");
-        final String[] start = split[0].split(",");
-        final String[] end = split[1].split(",");
-        mStartShortcut.setValue(start[0]);
-        mStartShortcut.setSummary(mStartShortcut.getEntry());
-        mEndShortcut.setValue(end[0]);
-        mEndShortcut.setSummary(mEndShortcut.getEntry());
-        mEnforceShortcut.setChecked(start.length == 1 && end.length == 1);
-    }
-
-    private void setShortcutSelection(String value, boolean start) {
-        setShortcutSelection(value, start, mEnforceShortcut.isChecked());
-    }
-
-    private void setShortcutSelection(String value, boolean start, boolean single) {
-        final String oldValue = getSettingsShortcutValue();
-        final int splitIndex = start ? 0 : 1;
-        String[] split = oldValue.split(";");
-        if (value.equals("none") || single) {
-            split[splitIndex] = value;
-        } else {
-            StringBuilder sb = new StringBuilder(value);
-            final String[] def = start ? DEFAULT_START_SHORTCUT : DEFAULT_END_SHORTCUT;
-            for (String str : def) {
-                if (str.equals(value)) continue;
-                sb.append(",").append(str);
-            }
-            split[splitIndex] = sb.toString();
-        }
-        Settings.System.putString(getActivity().getContentResolver(),
-                Settings.System.KEYGUARD_QUICK_TOGGLES_NEW, split[0] + ";" + split[1]);
-
-        if (start) {
-            mStartShortcut.setValue(value);
-            mStartShortcut.setSummary(mStartShortcut.getEntry());
-        } else {
-            mEndShortcut.setValue(value);
-            mEndShortcut.setSummary(mEndShortcut.getEntry());
-        }
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
