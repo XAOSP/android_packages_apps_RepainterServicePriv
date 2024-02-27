ee4e9089751143ffc48b8f0513b030591f961124
Martin J. Pollard <martin0566@gmail.com>
2024-02-26 22:10:34 -0500

    AltairSettings: Initial bringup for Altair ROM 14

--- src/com/altair/settings/fragments/CustomStatusBarSettings.java
+++ src/com/altair/settings/fragments/CustomStatusBarSettings.java
@@ -59,7 +59,7 @@ public class CustomStatusBarSettings extends DashboardFragment implements
     private static final String CATEGORY_BATTERY = "status_bar_battery_key";
     private static final String CATEGORY_CLOCK = "status_bar_clock_key";
 
-    private static final String KEY_USE_OLD_MOBILETYPE = "use_old_mobiletype";
+    //private static final String KEY_USE_OLD_MOBILETYPE = "use_old_mobiletype";
     private static final String KEY_DATA_DISABLED_ICON = "data_disabled_icon";
     private static final String KEY_ROAMING_INDICATOR_ICON = "roaming_indicator_icon";
     private static final String KEY_SHOW_FOURG_ICON = "show_fourg_icon";
@@ -80,7 +80,7 @@ public class CustomStatusBarSettings extends DashboardFragment implements
     private Context mContext;
     private ContentResolver mResolver;
 
-    private SwitchPreference mUseOldMobileType;
+    //private SwitchPreference mUseOldMobileType;
 
     private StatusBarIcon mClockIcon;
     private StatusBarIcon mBatteryIcon;
@@ -109,7 +109,7 @@ public class CustomStatusBarSettings extends DashboardFragment implements
         final PreferenceScreen prefScreen = getPreferenceScreen();
 
         final PreferenceCategory networkCategory = prefScreen.findPreference(CATEGORY_NETWORK);
-        mUseOldMobileType = findPreference(Settings.System.USE_OLD_MOBILETYPE);
+        //mUseOldMobileType = findPreference(Settings.System.USE_OLD_MOBILETYPE);
         SystemSettingSwitchPreference dataDisabledIcon = findPreference(KEY_DATA_DISABLED_ICON);
         SystemSettingSwitchPreference roamingIndicatorIcon =
                 findPreference(KEY_ROAMING_INDICATOR_ICON);
@@ -120,21 +120,21 @@ public class CustomStatusBarSettings extends DashboardFragment implements
                 findPreference(KEY_VOLTE_VOWIFI_OVERRIDE);
 
         if (!TelephonyUtils.isVoiceCapable(getActivity())) {
-            networkCategory.removePreference(mUseOldMobileType);
+            //networkCategory.removePreference(mUseOldMobileType);
             networkCategory.removePreference(dataDisabledIcon);
             networkCategory.removePreference(roamingIndicatorIcon);
             networkCategory.removePreference(showFourgIcon);
             networkCategory.removePreference(showVoLTEIcon);
             networkCategory.removePreference(showVoWiFiIcon);
             networkCategory.removePreference(voLTEvoWiFiOverride);
-        } else {
+        /*} else {
             boolean configUseOldMobileType = mContext.getResources().getBoolean(
                     com.android.internal.R.bool.config_useOldMobileIcons);
             boolean showing = Settings.System.getIntForUser(mResolver,
                     Settings.System.USE_OLD_MOBILETYPE,
                     configUseOldMobileType ? 1 : 0, UserHandle.USER_CURRENT) != 0;
             mUseOldMobileType.setChecked(showing);
-            mUseOldMobileType.setOnPreferenceChangeListener(this);
+            mUseOldMobileType.setOnPreferenceChangeListener(this);*/
         }
 
         mClockIcon = new StatusBarIcon(mContext, "clock");
@@ -219,10 +219,10 @@ public class CustomStatusBarSettings extends DashboardFragment implements
             case STATUS_BAR_BATTERY_STYLE:
                 enableStatusBarBatteryDependents(Integer.parseInt((String) newValue));
                 break;
-            case KEY_USE_OLD_MOBILETYPE:
+            /*case KEY_USE_OLD_MOBILETYPE:
                 Settings.System.putIntForUser(mResolver, Settings.System.USE_OLD_MOBILETYPE,
                         (Boolean) newValue ? 1 : 0, UserHandle.USER_CURRENT);
-                break;
+                break;*/
         }
         return true;
     }
